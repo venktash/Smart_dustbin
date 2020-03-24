2448f60ad4201d78950f76ccd7f5a1a6cbea0559
Venktash Kumar <venktashkmr9@gmail.com>
2020-03-24 14:07:45 +0530

    input source has been added in this

--- testing_code.ino
+++ testing_code.ino
@@ -1,13 +1,16 @@
 void setup() {
 pinMode(1,OUTPUT);  // put your setup code here, to run once:
   pinMode(2,OUTPUT);
+  pinMode(13,INPUT);
 
 }
 
 void loop() {
-  digitalWrite(1,HIGH);
+if(digiotalRread(13)==1)
+{  digitalWrite(1,HIGH);}
+  else{
   digitalWrite(1,LOW);
   digitalWrite(2,1);
-  // put your main code here, to run repeatedly:
+  }  // put your main code here, to run repeatedly:
 
 }
