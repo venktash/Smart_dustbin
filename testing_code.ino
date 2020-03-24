db222280f1b12e93c9434be2c4ec4475b6398819
Venktash Kumar <venktashkmr9@gmail.com>
2020-03-24 14:02:10 +0530

    some changes done

--- testing_code.ino
+++ testing_code.ino
@@ -1,11 +1,13 @@
 void setup() {
 pinMode(1,OUTPUT);  // put your setup code here, to run once:
+  pinMode(2,OUTPUT);
 
 }
 
 void loop() {
   digitalWrite(1,HIGH);
   digitalWrite(1,LOW);
+  digitalWrite(2,1);
   // put your main code here, to run repeatedly:
 
 }
