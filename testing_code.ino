ebaa37331bc6b978a76c1bfd0343305549903b35
Venktash Kumar <venktashkmr9@gmail.com>
2020-03-24 14:00:39 +0530

    my testing code

---
+++ testing_code.ino
@@ -0,0 +1,11 @@
+void setup() {
+pinMode(1,OUTPUT);  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  digitalWrite(1,HIGH);
+  digitalWrite(1,LOW);
+  // put your main code here, to run repeatedly:
+
+}
