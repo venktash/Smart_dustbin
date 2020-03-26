819b18f178f4febb49abe0daae6b355205e07861
venktash <venktashkmr9@gmail.com>
2020-03-26 18:13:48 +0530

    changes done

--- testing_code.ino
+++ testing_code.ino
@@ -1,16 +1,16 @@
 void setup() {
 pinMode(1,OUTPUT);  // put your setup code here, to run once:
-  pinMode(2,OUTPUT);
-  pinMode(13,INPUT);
+//  pinMode(2,OUTPUT);
+//  pinMode(13,INPUT);
 
 }
 
 void loop() {
 if(digiotalRread(13)==1)
-{  digitalWrite(1,HIGH);}
-  else{
-  digitalWrite(1,LOW);
-  digitalWrite(2,1);
-  }  // put your main code here, to run repeatedly:
+//{  digitalWrite(1,HIGH);}
+//  else{
+//  digitalWrite(1,LOW);
+//  digitalWrite(2,1);
+//  }  // put your main code here, to run repeatedly:
 
 }
